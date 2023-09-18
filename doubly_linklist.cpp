276f1eb0c58dabbf2cb5e341cc5871c0fee6b19f
Hamza Mehmood <mehmood.hamza777@gmail.com>
2023-09-19 01:32:22 +0500

    create node class

---
+++ doubly_linklist.cpp
@@ -0,0 +1,44 @@
+#include"doubly(header).h"
+
+int main()
+{
+Node* head;
+Node* tail;
+
+
+//create new node
+Node* node=new Node();
+//assign a value
+node->value=100;
+// a null to next
+node->next=NULL;
+// anull to previous
+node->previous=NULL;
+
+head=node;
+tail=node;
+node->next=NULL;
+  
+
+
+//second node
+
+node=new Node();
+node->value=200;
+node->next=NULL;
+//change
+//  next node to previous's tail means (nullptr) [now previous is no longer nullptr]
+node->previous=tail;
+
+//hi you(node's tail) previosly only one element so you have null now you have  
+// new next element so
+tail->next=node;
+tail=node;  
+
+
+
+ 
+
+
+return 0;    
+}
